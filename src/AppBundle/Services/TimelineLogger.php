bbe66e576fd82caa1e09998128236249b34a26d1
Diar Selimi <diarselimi92@gmail.com>
2016-11-19 16:42:06 +0100

    added the shelf logger for the timeline

--- src/AppBundle/Services/TimelineLogger.php
+++ src/AppBundle/Services/TimelineLogger.php
@@ -4,11 +4,29 @@ namespace AppBundle\Services;
 
 use AppBundle\Entity\Review;
 use AppBundle\Entity\Timeline;
+use AppBundle\Entity\UserBookShelf;
 use Doctrine\Common\EventSubscriber;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 
 class TimelineLogger implements EventSubscriber
 {
+    private $currentEntity = null;
+    private $timelineEntities = [
+        Review::class,
+        UserBookShelf::class
+    ];
+
+    private $timelineMessages = [
+        'review' => [
+            'title' => 'Book review',
+            'description' => '%s'
+        ],
+        'userbookshelf' => [
+            'title' => 'Shelf updated',
+            'description' => 'You added the book %s to the shelf %s'
+        ]
+    ];
+
     public function getSubscribedEvents()
     {
         return array(
@@ -31,19 +49,55 @@ class TimelineLogger implements EventSubscriber
     {
         $entity = $args->getEntity();
         // for now I am logging only the review for the book
-        if ($entity instanceof Review) {
+        if ($this->isForTimeline($entity)) {
             $em = $args->getEntityManager();
             $timeline = new Timeline();
-            $timeline->setTitle('Book review');
-            $timeline->setDescription($entity->getReview());
+            $timeline->setTitle($this->getTitle());
+
+            $second = null;
+            if($entity instanceof Review) {
+                $desc = $entity->getReview();
+            }else{
+                $desc = $entity->getBook()->getTitle();
+                $second = $entity->getShelf()->getName();
+            }
+
+            $timeline->setDescription($this->getDescription($desc, $second));
             $timeline->setTable('Review');
             $timeline->setParentId($entity->getBook()->getId());
             $timeline->setLink('no link');
             $timeline->setUserId($entity->getUser()->getId());
-
+            
             $em->persist($timeline);
             $em->flush();
 
         }
+    }
+
+    /**
+     * This function will check if the entity will be added in the timeline event.
+     * @param $newEntity
+     * @return bool
+     */
+    protected function isForTimeline($newEntity)
+    {
+        foreach ($this->timelineEntities as $entity) {
+            $entity = new $entity();
+            if($entity instanceof $newEntity) {
+                $this->currentEntity = (new \ReflectionClass($newEntity))->getShortName();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected function getTitle()
+    {
+        return $this->timelineMessages[strtolower($this->currentEntity)]['title'];
+    }
+
+    protected function getDescription($val1, $val2 = null)
+    {
+        return sprintf($this->timelineMessages[strtolower($this->currentEntity)]['description'], $val1, $val2);
     }
 }
